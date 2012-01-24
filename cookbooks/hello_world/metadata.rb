f0e48352132b491bd2cee816a5635dceaae3f5b7
Ryan J. Geyer <me@ryangeyer.com>
2012-01-24 14:09:27 -0800

    Added hello_world cookbook

---
+++ cookbooks/hello_world/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RightScale"
+maintainer_email "ryan.geyer@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures hello_world"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "hello_world::default", "Prints hello world to the audit entry"
---
+++ cookbooks/hello_world/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: hello_world
+# Recipe:: default
+#
+# Copyright 2012, RightScale
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello world!"
