6a9e435398a67c797150500354236611d1e0a72d
Ryan J. Geyer <me@ryangeyer.com>
2012-01-24 15:54:33 -0800

    Added httpd cookbook

--- cookbooks/httpd/recipes/install_httpd.rb
+++ cookbooks/httpd/recipes/install_httpd.rb
@@ -1,10 +1,12 @@
 #
-# Cookbook Name:: hello_world
-# Recipe:: default
+# Cookbook Name:: httpd
+# Recipe:: install_httpd
 #
 # Copyright 2012, RightScale <ryan.geyer@rightscale.com>
 #
 # All rights reserved - Do Not Redistribute
 #
 
-log "Hello world!"
+package "apache2" do
+  action :install
+end
